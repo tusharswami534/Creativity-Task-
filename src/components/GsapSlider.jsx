d79b805c3a25fe0fdfb9213d71fd47b1505f88c3
tusharswami534 <tusharswami17@gmail.com>
2025-01-02 13:55:10 +0530

    complete slider

--- src/components/GsapSlider.jsx
+++ src/components/GsapSlider.jsx
@@ -16,10 +16,11 @@ const GsapSlider = () => {
           end: "bottom top",
           scrub: 1,
           pin: true,
-          markers: true,
         },
       });
+
 // ================================First animation================================
+
       tl.to(".card-one",{
           yPercent: 50,
           xPercent: 100,
@@ -28,7 +29,7 @@ const GsapSlider = () => {
         },"start-one");
 
         tl.to(".controller-one",{
-          y: -500,
+          y: -600,
           duration: 1,
           ease: "easeInOut",
         }, "start-one");
@@ -75,17 +76,16 @@ const GsapSlider = () => {
         "start-two");
 
       tl.to(".controller-one",{
-        y: -1000,
+        y: -1300,
         duration: 1,
         ease: "easeInOut",
       }, "start-two");
 
       tl.to(".controller-two",{
-        y: -500,
+        y: -600,
         duration: 1,
         ease: "easeInOut",
       }, "start-two");
-
 
 // ================================================Third animation================================
 
@@ -122,13 +122,13 @@ const GsapSlider = () => {
         "start-three");
 
       tl.to(".controller-two",{
-        y: -1000,
+        y: -1300,
         duration: 1,
         ease: "easeInOut",
       }, "start-three");
 
       tl.to(".controller-three",{
-        y: -500,
+        y: -600,
         duration: 1,
         ease: "easeInOut",
       }, "start-three");
@@ -144,8 +144,8 @@ const GsapSlider = () => {
         "start-four");
 
       tl.to(".card-three",{
-        yPercent: 155,
-        xPercent: 30,
+        yPercent: 190,
+        xPercent: 50,
         duration: 1,
         ease: "easeInOut",
         },
@@ -160,13 +160,13 @@ const GsapSlider = () => {
         "start-four");
 
       tl.to(".controller-three",{
-        y: -1000,
+        y: -1300,
         duration: 1,
         ease: "easeInOut",
       }, "start-four");
 
       tl.to(".controller-four",{
-        y: -500,
+        y: -600,
         duration: 1,
         ease: "easeInOut",
       }, "start-four");
@@ -176,7 +176,6 @@ const GsapSlider = () => {
   }, []);
 
   return (
-    
     <div ref={componentRef} className="overflow-hidden  bg-gray-200 mx-auto">
      <div className="max-w-[1536px] mx-auto">
      <div className="min-h-screen flex justify-center items-center bg-gray-200">
@@ -187,13 +186,13 @@ const GsapSlider = () => {
         <div className="card card-one w-80 h-[500px] z-50 backdrop-filter  absolute flex justify-center items-center translate-y-[-50%] top-[-10%] left-[-10%]">
           <img className="backdrop-shadow" src="./assets/image/png/controller-1.png" alt="controller" />
         </div>
-        <div className="card card-two w-80 h-[500px] z-40 backdrop-blur-sm bg-blue-400 absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
+        <div className="card card-two w-80 h-[500px] z-40 backdrop-blur-sm bg-transparent absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
           <img className="backdrop-shadow" src="./assets/image/png/controller-2.png" alt="controller" />
         </div>
-        <div className="card card-three w-80 h-[500px] backdrop-blur-sm bg-green-400 absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
+        <div className="card card-three w-80 h-[500px] backdrop-blur-sm bg-transparent absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
           <img className="backdrop-shadow" src="./assets/image/png/controller-3.png" alt="" />
         </div>
-        <div className="card card-four w-80 h-[500px] backdrop-blur-sm bg-yellow-400 absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
+        <div className="card card-four w-80 h-[500px] backdrop-blur-sm bg-transparent absolute flex justify-center items-center translate-y-[-50%] top-[-20%] left-[-20%]">
           <img className="backdrop-shadow" src="./assets/image/png/controller-4.png" alt="" />
         </div>
         </div>
@@ -207,20 +206,19 @@ const GsapSlider = () => {
                 <h1 className="text-6xl uppercase font-sohne mb-2">DualSense</h1>
                 <p className="text-2xl">The controller for PlayStation 5, featuring adaptive triggers and haptic feedback for a more immersive gaming experience.</p>
             </div>
-
             <div className="absolute controller-three top-[110%] right-0 w-[500px] transform -translate-x-1/2 -translate-y-1/2">
                 <h1 className="text-6xl uppercase font-sohne mb-2">DualShock 3</h1>
                 <p className="text-2xl">The controller for PlayStation 3, featuring motion sensing and wireless gameplay.</p>
             </div>
-
             <div className="absolute controller-four top-[110%] right-0 w-[500px] transform -translate-x-1/2 -translate-y-1/2">
                 <h1 className="text-6xl uppercase font-sohne mb-2">PS Move Controller</h1>
                 <p className="text-2xl">A motion-sensing controller used for PlayStation VR and certain PS3 and PS4 games.</p>
             </div>
         </div>
       </div>
-      <div className="min-h-screen flex justify-center items-center bg-gray-200">
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-200">
           <h1 className="text-9xl uppercase font-sohne">thank  You</h1>
+          <p className="text-3xl ">This Slider only for 1440 screen</p>
       </div>
      </div>
     </div>
